5db6ef9e913dca56c5e9ace0752524565649d662
iwsandfo <iwsandfo@mtu.edu>
2022-04-21 15:55:24 -0400

    More updates to login page
    Removed the gross purple bars from the top of the login page. I also locked the login screen to portrait mode, because some of the buttons would wind up off-screen in landscape mode.

--- app/src/main/java/com/example/tsp7calculator/data.java
+++ app/src/main/java/com/example/tsp7calculator/data.java
@@ -3,9 +3,12 @@ package com.example.tsp7calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -23,6 +26,12 @@ public class data extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
+
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
 
         b1 = findViewById(R.id.button);
         ed1 = findViewById(R.id.editText);
